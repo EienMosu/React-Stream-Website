05d41f75a0f9072d4f908c3d8357ccc17773af6d
EienMosu <82905592+EienMosu@users.noreply.github.com>
2021-10-01 00:26:42 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default App
