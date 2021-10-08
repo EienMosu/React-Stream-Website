fe357fb212d68d0eb3c3d46777fb04731891174d
EienMosu <82905592+EienMosu@users.noreply.github.com>
2021-10-08 19:50:42 +0200

    Add files via upload

--- src/pages/StreamCreate/index.jsx
+++ src/pages/StreamCreate/index.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+import {
+  Button,
+  Container,
+  Form,
+  Input,
+  Label,
+  Span,
+} from "./StreamCreate.styles";
 
-const StreamCreate = () => {
+
+export class StreamCreate extends Component {
+    
+  renderInput({ input, label, meta }) {
     return (
-        <div>
-            StreamCreate
-        </div>
-    )
+      <>
+        <Label>{label}</Label>
+        <Input {...input} />
+        <Span>{meta.touched && meta.error}</Span>
+      </>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <Container>
+        <Form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+          <Field
+            name="title"
+            component={this.renderInput}
+            label="Enter Title"
+          />
+          <Field
+            name="description"
+            component={this.renderInput}
+            label="Enter Description"
+          />
+          <Button>Submit</Button>
+        </Form>
+      </Container>
+    );
+  }
 }
 
-export default StreamCreate
+const validate = (formValues) => {
+    const errors = {};
+
+    if (!formValues.title) {
+        errors.title = "You must enter a title!"
+    }
+
+    if (!formValues.description) {
+        errors.description = "You must enter a description!"
+    }
+
+    return errors;
+}
+
+export default reduxForm({
+  form: "streamCreate",
+  validate
+})(StreamCreate);
