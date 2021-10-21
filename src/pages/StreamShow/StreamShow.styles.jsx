9d0430ceb72835a24a3e851c0129eeffa0efc347
EienMosu <82905592+EienMosu@users.noreply.github.com>
2021-10-21 03:51:38 +0200

    Add files via upload

--- src/pages/StreamShow/StreamShow.styles.jsx
+++ src/pages/StreamShow/StreamShow.styles.jsx
@@ -1,7 +1,22 @@
 import styled from "styled-components";
 
 export const Container = styled.div `
-    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Wrapper = styled.div `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 90%;
+`
+export const Video = styled.video `
+    width: 70%;
+    margin: 30px 0px;
+    border-radius: 30px;
 `
 
 export const Title = styled.h3 `
@@ -10,4 +25,5 @@ export const Title = styled.h3 `
 
 export const Desc = styled.span `
     font-size: 24px;
+    margin: 10px 0px;
 `
--- src/pages/StreamShow/index.jsx
+++ src/pages/StreamShow/index.jsx
@@ -1,14 +1,47 @@
 import React, { Component } from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../redux/actions";
-import { Container, Desc, Title } from "./StreamShow.styles";
+import { Container, Desc, Title, Video, Wrapper } from "./StreamShow.styles";
 
 class StreamShow extends Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
     var myPath = window.location.pathname;
     myPath = myPath.split("/").pop();
 
     this.props.fetchStream(myPath);
+
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    var myPath = window.location.pathname;
+    myPath = myPath.split("/").pop();
+
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${myPath}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
@@ -18,8 +51,11 @@ class StreamShow extends Component {
 
     return (
       <Container>
-        <Title>{this.props.stream.title}</Title>
-        <Desc>{this.props.stream.description}</Desc>
+        <Wrapper>
+          <Video ref={this.videoRef} controls={true} />
+          <Title>{this.props.stream.title}</Title>
+          <Desc>{this.props.stream.description}</Desc>
+        </Wrapper>
       </Container>
     );
   }
